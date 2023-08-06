29d95b1fd0a7ae2267908b30986a6130ae023c72
e0401368543 <138401719+e0401368543@users.noreply.github.com>
2023-08-05 21:23:48 -0500

    MDI llamado

--- QuizMaster.UI.Windows/Forms/Loggin.cs
+++ QuizMaster.UI.Windows/Forms/Loggin.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QuizMaster.UI.Windows.ApplicationController;
+using QuizMaster.UI.Windows.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +12,42 @@ using System.Windows.Forms;
 
 namespace QuizMaster.UI.Windows.Forms
 {
-    public partial class Loggin : Form
+    public partial class Loggin : MetroFramework.Forms.MetroForm
     {
+            public UsuariosViewModel viewModelUsuarios;
+        public UsuariosController controllerUsuarios;
         public Loggin()
         {
             InitializeComponent();
+            viewModelUsuarios=new UsuariosViewModel();
+            controllerUsuarios= new UsuariosController();
+        }
+
+        private void Loggin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+           if(controllerUsuarios.BuscarUsusarios(mtxtUsuarios.Text, mtxtUsuariocontraseña.Text))
+            {
+
+                MessageBox.Show("Ususario activo");
+                MDIQuizMaster childFormPrincipal = new MDIQuizMaster();
+                //childFormCategoria.MdiParent = this;
+                childFormPrincipal.Show();
+                this.Close();
+                
+
+            }
+
         }
     }
 }
--- QuizMaster.UI.Windows/Program.cs
+++ QuizMaster.UI.Windows/Program.cs
@@ -17,7 +17,7 @@ namespace QuizMaster.UI.Windows
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new IngresoAlumnos());
+            Application.Run(new Loggin());
         }
     }
 }
